9cb2f0aaf757fddb8f566df7ed2fd9484d223af3
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-01-04 06:27:06 +0530

    Add failing test case for single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,5 +1,9 @@
-const add = require('../src/stringCalculator');
+const add = require("../src/stringCalculator");
 
-test('returns 0 for an empty string', () => {
+test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns number for a single element in the string", () => {
+  expect(add("1")).toBe(1);
 });
