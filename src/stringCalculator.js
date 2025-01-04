6135c6cde7fd502615c17a28e7b6134bb0e5566c
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-01-04 07:10:47 +0530

    Add logic to handle custom delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,14 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const numbersArr = numbers.replace("\n", ",").split(",");
 
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(`[${parts[0].slice(2)}]`); // take part before \n, and remove "//" to get delimiter
+    numbers = parts[1];
+  }
+
+  const numbersArr = numbers.split(delimiter);
   const sum = numbersArr.reduce((total, num) => total + parseInt(num, 10), 0);
   return sum;
 }
