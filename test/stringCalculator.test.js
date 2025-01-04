93e8106deed80b0510a1f1199cc6df89dfff6e7e
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-01-04 06:42:20 +0530

    Add another test case for two comma separated numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -9,6 +9,7 @@ test("returns number for a single element in the string", () => {
   expect(add("2")).toBe(2);
 });
 
-test("comma separated numbers should return their sum", () => {
+test("two comma separated numbers should return their sum", () => {
   expect(add("1,3")).toBe(4);
+  expect(add("1,5")).toBe(6);
 });
